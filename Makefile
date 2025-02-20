243acf8aad690e97f85ae6fb2557afcff00592dc
khieem <ngkhiem47@gmail.com>
2025-02-20 03:03:16 +0000

    unshare uts/pid namespace

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 hocker: hocker.c
 	$(CC) $^ -o $@
+
+debug: hocker.c
+	$(CC) -g $^ -o $@
--- hocker.c
+++ hocker.c
@@ -1,12 +1,57 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sched.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
+void unshare_namespace()
+{
+	const int FLAGS = CLONE_NEWUTS|CLONE_NEWPID;
+
+	if (unshare(FLAGS) == -1)	
+	{
+		perror("unshare");
+		exit(EXIT_FAILURE);
+	}
+
+	// TODO
+	// mount /proc in new namespace
+}
+
+// wrapper for exec()
+// TODO initialize env
+void start_container(char *path, char **argv, char** envp)
+{
+	char hostname[] = "container";
+	sethostname(hostname, sizeof(hostname)/sizeof(*hostname));
+	// fprintf(stderr, "%zd %zd", sizeof(hostname), sizeof(*hostname));
+
+	execve(path, argv, envp);
+}
+
 void hocker(char *path, char **argv)
 {
-	execve(path, argv, NULL);
+	unshare_namespace();
+
+	pid_t pid = fork();
+	switch (pid)
+	{
+	case -1:
+		perror("fork");
+		exit(EXIT_FAILURE);
+	case 0:
+		start_container(path, argv, NULL);
+		break;
+	default:
+		int status;
+		wait(&status);
+		exit(EXIT_SUCCESS);
+	}
 }
 
 int main(int argc, char *argv[])
@@ -17,18 +62,6 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	pid_t pid = fork();
-	switch (pid)
-	{
-	case -1:
-		perror("fork");
-		exit(EXIT_FAILURE);
-	case 0:
-		hocker(argv[1], &argv[1]);
-		break;
-	default:
-		int status;
-		wait(&status);
-		exit(EXIT_SUCCESS);
-	}
+	hocker(argv[1], &argv[1]);
+	exit(EXIT_SUCCESS);
 }
